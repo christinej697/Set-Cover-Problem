ee7aeaa769405a34adaf2954c61ef48854fb12a1
Christine Johnson <49726302+christinej697@users.noreply.github.com>
2022-04-07 19:13:10 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# Takes in the set U of n elements and a list of subsets U S1,...,S2
+# Returns a list of sets included in the set cover
+def set_cover(u_set, s_list):
+    r_set = u_set
+    print(f'Orig R set: {r_set}')
+    print(f'List of subsets: {s_list}')
+    print()
+    select_set = []
+    while len(r_set) != 0:
+        elems = 0
+        for s in s_list:
+            if elems < len(r_set.intersection(s)):
+                elems = len(r_set.intersection(s))
+                select = s
+        print(f'Selected s {select}')
+        select_set.append(select)
+        print(f'R before: {r_set}')
+        r_set = r_set - select
+        print(f'R minus s: {r_set}')
+        print()
+    return select_set
+
+if __name__ == '__main__':
+    test1 = [{1,2,3,8,9,10},{1,2,3,4,5},{4,5},{6},{4,5,7},{5,6,7},{6,7,8,9,10}]
+    test2 = [{1,2,3},{2,4},{3,4},{4,5}]
+    print("Test 1: ")
+    print(set_cover({1,2,3,4,5,6,7,8,9,10},test1))
+    print()
+    print()
+    print("Test 2: ")
+    print(set_cover({1,2,3,4,5},test2))
